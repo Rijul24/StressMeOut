d8cb81e83177b1e0da70e007e8b2d8e2a4c674be
armaanbadhan <66007619+armaanbadhan@users.noreply.github.com>
2021-04-11 12:28:02 +0530

    Update prefixes.py

--- cogs/prefixes.py
+++ cogs/prefixes.py
@@ -1,8 +1,5 @@
-import discord
 from discord.ext import commands
 import json
-from exttt.myembeds import whatpref
-from asyncio import sleep
 
 
 class Prefixes(commands.Cog):
@@ -27,19 +24,16 @@ class Prefixes(commands.Cog):
 
     @commands.command(aliases=["changeprefix"])
     @commands.has_permissions(administrator=True)
-    async def changepref(self, ctx, pref):
+    async def changepref(self, ctx, pref=None):
+        if pref is None:
+            ctx.send("Prefix is a required argument which is missing")
+            return
         with open("prefixes.json", "r") as f:
             prefixes = json.load(f)
         prefixes[str(ctx.guild.id)] = pref
         with open("prefixes.json", "w") as f:
             json.dump(prefixes, f)
         await ctx.send(f"new prefix is {pref}")
-
-    @commands.command()
-    async def prefix(self, ctx):
-        async with ctx.typing():
-            await sleep(0)
-        await ctx.send(embed=whatpref(ctx))
 
 
 def setup(client):
